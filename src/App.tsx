01dd76f2c354d37d3da4da5ee5d118a972c1964c
pessoadavi <pessoadavi@gmail.com>
2023-07-31 00:19:37 -0300

    Aplicação da biblioteca react-router-dom e criação das rotas, Aplicação da bilio phorphor-react para as iamgens e estilização inicial da tela do timer.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Button } from "./components/Button";
 import { ThemeProvider } from "styled-components";
 
-import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
