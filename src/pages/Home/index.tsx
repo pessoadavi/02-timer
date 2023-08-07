ebb645d52004d85ce0e7c6703daa61737439c756
pessoadavi <pessoadavi@gmail.com>
2023-08-07 00:39:50 -0300

    conclusão tela home.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,42 @@
+import { Play } from "phosphor-react";
+import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountDownButton, TaskInput } from "./styles";
+
+export const Home = () => {
+    return (
+        <>
+            <HomeContainer>
+                <form action="">
+                    <FormContainer>
+                        <label htmlFor="task">Vou trabalhar em </label>
+                        <TaskInput
+                            id="task"
+                            placeholder="Dê um nome para o seu projeto"
+                        />
+
+                        <label htmlFor="">durante</label>
+                        <MinutesAmountInput
+                            id="minutesAmount"
+                            type="number"
+                            placeholder="00"
+                        />
+
+                        <span>minutos.</span>
+                    </FormContainer>
+
+                    <CountDownContainer>
+                        <span>0</span>
+                        <span>0</span>
+                        <Separator>:</Separator>
+                        <span>0</span>
+                        <span>0</span>
+                    </CountDownContainer>
+
+                    <StartCountDownButton disabled={false} type="submit">
+                        <Play size={24} />
+                        Começar
+                    </StartCountDownButton>
+                </form>
+            </HomeContainer>
+        </>
+    );
+}
