6eff8049b5c125b6156fa03d6ef716e3e08afe1a
pessoadavi <pessoadavi@gmail.com>
2023-09-15 20:38:11 -0300

    finalizando estilização do status.

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles"
+import { HistoryContainer, HistoryList, Status } from "./styles"
 
 export const History = () => {
     return (
@@ -6,48 +6,51 @@ export const History = () => {
             <h1>History</h1>
 
             <HistoryList>
-            <table>
-                <thead>
-                    <tr>
-                       <th>Tarefa</th>
-                       <th>Duração</th>
-                       <th>Início</th>
-                       <th>Status</th> 
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Tarefas</td>
-                        <td>20 minutos</td>
-                        <td>Há dois meses</td>
-                        <td>Concluído</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>Tarefas</td>
-                        <td>20 minutos</td>
-                        <td>Há dois meses</td>
-                        <td>Concluído</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>Tarefas</td>
-                        <td>20 minutos</td>
-                        <td>Há dois meses</td>
-                        <td>Concluído</td>
-                    </tr>
-                </tbody>
-                <tbody>
-                    <tr>
-                        <td>Tarefas</td>
-                        <td>20 minutos</td>
-                        <td>Há dois meses</td>
-                        <td>Concluído</td>
-                    </tr>
-                </tbody>
-            </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Tarefas</td>
+                            <td>20 minutos</td>
+                            <td>Há dois meses</td>
+                            <td><Status statusColor="green"> Concluído </Status></td>
+                        </tr> 
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Tarefas</td>
+                            <td>20 minutos</td>
+                            <td>Há dois meses</td>
+                            <td><Status statusColor="green"> Concluído </Status></td>
+
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Tarefas</td>
+                            <td>20 minutos</td>
+                            <td>Há dois meses</td>
+                            <td><Status statusColor="green"> Concluído </Status></td>
+
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>Tarefas</td>
+                            <td>20 minutos</td>
+                            <td>Há dois meses</td>
+                            <td><Status statusColor="green"> Concluído </Status></td>
+
+                        </tr>
+                    </tbody>
+                </table>
             </HistoryList>
         </HistoryContainer>
     );
